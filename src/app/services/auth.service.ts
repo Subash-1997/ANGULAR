d43fefbfcc06a44bdcd628b15ff3f2befbc48f59
Subash-1997 <139530532+Subash-1997@users.noreply.github.com>
2023-08-15 16:35:06 -0700

    Add files via upload

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private router: Router) { }
+
+  login(uname: string, pward: string) {
+    if (uname === 'suren' && pword === '1234') {
+      return 200;
+    } else {
+      return 403;
+    }
+  }
+  logout() {
+    this.router.navigate(['login'])
+
+  }
+}
